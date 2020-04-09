e24eafcc6d4cd46ce06626cc250c03111be183be
Prerna <prerna@Prernas-MacBook-Pro.local>
2020-04-09 13:01:39 -0400

    design pattern

--- designPattern/iterator.js
+++ designPattern/iterator.js
@@ -1,3 +1,34 @@
 //Provide a way to access the elements of an aggregate object sequentially
 // without exposing its underlying representation.
 
+var iterator = (function(i) {
+        let index = i;
+		data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+		length = data.length;
+
+	return {
+		next: function() {
+			var element;
+			if (!this.hasNext()) {
+				return null;
+			}
+			element = data[index];
+			index += 1;
+			return element;
+		},
+		hasNext: function() {
+			return index < length;
+		},
+		rewind: function() {
+			index = 0;
+			return data[index];
+		},
+		current: function() {
+			return data[index];
+		}
+	}
+
+}(2));
+
+console.log(iterator.next());
+console.log(iterator.hasNext());
--- designPattern/singleton.js
+++ designPattern/singleton.js
@@ -19,3 +19,26 @@ const ref1 = S.getInstance();
 const ref2 = S.getInstance();
 
 console.log(ref1 === ref2);
+
+class S1 {
+    constructor() {
+        this.instance;
+    }
+
+    getInstance() {
+        if (!this.instance) {
+            this.createObject();
+        }
+        else {
+            return this.instance;
+        }
+    }
+    createObject() {
+        let obj = new Object();
+        return obj;
+    }
+}
+
+const r1 = new S1();
+const r2 = new S1();
+console.log(r1.getInstance() === r2.getInstance());
